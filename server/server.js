450eb759c0e03edc4255bdf5b5ac7c52aeec112a
Gaurav Jain <2022kuec2013@iiitkota.ac.in>
2025-01-17 00:53:33 +0530

    before payment integration

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 
 // App config
@@ -20,5 +21,7 @@ app.use(cors())
 
 app.get('/',(req,res)=> res.send("API working"))
 app.use('/api/user', userRouter)
+app.use('/api/image', imageRouter)
+
 
 app.listen(PORT, ()=>console.log("server running on port " + PORT));
